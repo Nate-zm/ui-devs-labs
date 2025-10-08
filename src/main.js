831cf37802e552ab0cf6837079ceaae888500cf4
Nate-zm <160774122+Nate-zm@users.noreply.github.com>
2025-10-08 22:23:07 +0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import HeaderComponent  from './view/header-component.js';
+import FormAddTaskComponent from './view/form-add-task-component.js';
+import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
+import {render, RenderPosition} from './framework/render.js';
+import TasksModel from './model/task-model.js';
+
+const bodyContainer= document.querySelector('.app');
+const formContainer = document.querySelector('.form-container');
+const taskBoardContainer = document.querySelector('.task-board');
+const tasksModel = new TasksModel();
+
+const tasksBoardPresenter = new TasksBoardPresenter({boardContainer: taskBoardContainer,tasksModel,})
+
+render(new HeaderComponent(), bodyContainer, RenderPosition.AFTERBEGIN);
+render(new FormAddTaskComponent(), formContainer, RenderPosition.AFTERBEGIN);
+
+tasksBoardPresenter.init();
---
+++ src/model/task-model.js
@@ -0,0 +1,9 @@
+import { tasks } from '../mock/task.js'
+
+export default class TaskModel{
+    #boardTasks = tasks;
+
+    get tasks(){
+        return this.#boardTasks;
+    }
+}
