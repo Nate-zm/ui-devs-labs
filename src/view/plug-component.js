bfce3a1e21908f55b889356cd031fc614c4250c9
Nate-zm <160774122+Nate-zm@users.noreply.github.com>
2025-11-12 20:28:49 +0300

    Add files via upload

---
+++ src/view/plug-component.js
@@ -0,0 +1,20 @@
+import {createElement} from '../framework/render.js'; 
+import { AbstractComponent } from '../framework/view/abstract-component.js';
+
+
+function createHeaderComponentTemplate() {
+    return (
+        `<li class = "tasks_plug">Перетащите карточку</li>`      
+    );
+}
+
+
+export default class PlugComponent extends AbstractComponent{
+    constructor(){
+      super();
+    }
+
+  get template(){
+        return createHeaderComponentTemplate();
+  }
+}
